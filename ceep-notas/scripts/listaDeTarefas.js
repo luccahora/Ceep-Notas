71519dc0ce181c38cdcd833d29b2dbd6a514b58a
Lucca Hora <luccahora@gmail.com>
2020-07-11 04:45:15 -0300

    Adicionando IIFE JS

--- ceep-notas/scripts/listaDeTarefas.js
+++ ceep-notas/scripts/listaDeTarefas.js
@@ -1,27 +1,28 @@
-const criarTarefa = (evento) => {
-    evento.preventDefault()
-    
-    const lista = document.querySelector('[data-list]')
-    const input = document.querySelector('[data-form-imput]')
-    const valor = input.value
-    
-    const tarefa = document.createElement('li')
-    tarefa.classList.add('task')
+(() => {   
+    const criarTarefa = (evento) => {
+        evento.preventDefault()
+        
+        const lista = document.querySelector('[data-list]')
+        const input = document.querySelector('[data-form-imput]')
+        const valor = input.value
+        
+        const tarefa = document.createElement('li')
+        tarefa.classList.add('task')
 
-    const conteudo = `<p class="content"> ${valor} </p>`
+        const conteudo = `<p class="content"> ${valor} </p>`
 
-    tarefa.innerHTML = conteudo;
+        tarefa.innerHTML = conteudo;
 
-    tarefa.appendChild(BotaoConclui())
-    lista.appendChild(tarefa)
-    input.value = ""
-}
+        tarefa.appendChild(BotaoConclui())
+        lista.appendChild(tarefa)
+        input.value = ""
+    }
 
-const novaTarefa = document.querySelector('[data-form-button]');
+    const novaTarefa = document.querySelector('[data-form-button]');
 
-novaTarefa.addEventListener("click", (criarTarefa))
+    novaTarefa.addEventListener("click", (criarTarefa))
 
-const BotaoConclui = () =>{
+    const BotaoConclui = () =>{
     const botaoConclui = document.createElement('button');
 
     botaoConclui.classList.add('check-button')
@@ -32,10 +33,11 @@ const BotaoConclui = () =>{
     return botaoConclui
 }
 
-const concluirTarefa = (evento) =>{
+const concluirTarefa = (evento) => {
     const botaoConclui = evento.target
 
     const tarefaCompleta = botaoConclui.parentElement
 
     tarefaCompleta.classList.toggle('done')
-}
+    }
+})()
